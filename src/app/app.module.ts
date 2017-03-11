e636cc2b56750cbe10df1349cd150d0b1315c5b2
FCACERES <ferneycaceres@gmail.com>
2017-03-11 10:32:09 -0500

    modulo dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 import { AlertModule } from 'ng2-bootstrap';
+import { DropdownModule } from 'ng2-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +16,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    DropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
