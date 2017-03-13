f19aa3191a67ed18fd66a31c3dda01bed471e63f
unknown <ferneycaceres@gmail.com>
2017-03-13 16:13:33 -0500

    commit firebase json

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,7 +7,7 @@ export class DataService {
   constructor(private http: Http ) { }
 
   fetchData(){
-    return this.http.get('/src/ninjas.json').map(
+    return this.http.get('https://ng-angular-68038.firebaseio.com/.json').map(
       (res) => res.json()
     );
   }
