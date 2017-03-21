b64ccf0a323fc33b5178d843b7128e3a3b930582
FCACERES <ferneycaceres@gmail.com>
2017-03-21 17:55:01 -0500

    mock updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,19 +17,12 @@ export class AppComponent {
   plans = [];
 
   public selected:string;
-  public states:string[] = ['Alabama', 'Alaska', 'Arizona', 'Arkansas',
-    'California', 'Colorado',
-    'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho',
-    'Illinois', 'Indiana', 'Iowa',
-    'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts',
-    'Michigan', 'Minnesota',
-    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-    'New Jersey', 'New Mexico',
-    'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon',
-    'Pennsylvania', 'Rhode Island',
-    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-    'Virginia', 'Washington',
-    'West Virginia', 'Wisconsin', 'Wyoming'];
+  public products:string[] = ['El Clasico de España R.Madrid vs FCBarcelona',
+                              'FA Cup Final : Manchester United vs Chelsea',
+                              'Community Shield Final : Manchester City vs Everton',
+                              'Final Copa Colombia : Atletico Bucaramanga vs DIM',
+                              'Final ATP 500 Buenos Aires',
+                              'Final USA Open'];
 
   constructor (private dataService : DataService){}
 
